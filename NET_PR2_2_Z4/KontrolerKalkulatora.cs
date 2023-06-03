95fb1b544d79da0fd0a05c2a2e43607578f4419a
PMBytner <pawel.m.bytner@gmail.com>
2023-06-03 12:19:20 +0200

    Dodaj pliki projektów.

---
+++ NET_PR2_2_Z4/KontrolerKalkulatora.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_PR2_2_Z4;
+internal class KontrolerKalkulatora : INotifyPropertyChanged
+{
+	private double?
+		lewyArgument = null,
+		prawyArgument = null
+		;
+	private string?
+		buforDziałania = null,
+		wynik = "0"
+		;
+
+	public string Wynik {
+		get => wynik;
+		set { 
+			wynik = value;
+			PropertyChanged?.Invoke(
+				this,
+				new PropertyChangedEventArgs("Wynik")
+				);
+		}
+	}
+	internal void WprowadźCyfrę(string cyfra)
+	{
+		if (Wynik == "0")
+			Wynik = cyfra;
+		else
+			Wynik += cyfra;
+	}
+	internal void ZmieńZnak()
+	{
+		if (Wynik == "0")
+			return;
+		else if (Wynik[0] == '-')
+			Wynik = Wynik.Substring(1);
+		else
+			Wynik = "-" + Wynik;
+	}
+	internal void WprowadźPrzecinek()
+	{
+		if (Wynik.Contains(','))
+			return;
+		else
+			Wynik += ",";
+	}
+
+	internal void SkasujZnak()
+	{
+		if (Wynik == "0")
+			return;
+		else if (
+			Wynik == "-0,"
+			|| Wynik.Length == 1
+			|| (Wynik.Length == 2 && Wynik[0] == '-')
+			)
+			Wynik = "0";
+		else
+			Wynik = Wynik.Substring(0,Wynik.Length-1);
+	}
+
+	internal void WyczyśćWynik()
+	{
+		Wynik = "0";
+	}
+
+	internal void WyczyśćWszystko()
+	{
+		WyczyśćWynik();
+		lewyArgument = prawyArgument = null;
+		buforDziałania = null;
+	}
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+}
---
+++ NET_PR2_2_Z4/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NET_PR2_2_Z4;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	KontrolerKalkulatora Kontroler = new();
+	public MainWindow()
+	{
+		DataContext= Kontroler;
+		InitializeComponent();
+	}
+
+	private void Cyfra(object sender, RoutedEventArgs e)
+	{
+		Kontroler.WprowadźCyfrę(
+			((Button)sender).Content.ToString()
+			);
+	}
+
+	private void Znak(object sender, RoutedEventArgs e)
+	{
+		Kontroler.ZmieńZnak();
+	}
+
+	private void Przecinek(object sender, RoutedEventArgs e)
+	{
+		Kontroler.WprowadźPrzecinek();
+	}
+
+	private void KasowanieZnaku(object sender, RoutedEventArgs e)
+	{
+		Kontroler.SkasujZnak();
+	}
+
+	private void CzyszczenieWyniku(object sender, RoutedEventArgs e)
+	{
+		Kontroler.WyczyśćWynik();
+	}
+
+	private void Czyszczenie(object sender, RoutedEventArgs e)
+	{
+		Kontroler.WyczyśćWszystko();
+	}
+}
